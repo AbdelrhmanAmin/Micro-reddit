6a702b1002aa9647aaac55b5c1147f28c2006cca
AbdelrhmanAmin <maxhd213@gmail.com>
2021-01-03 00:21:09 +0200

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {in: 3..12}
+  validates :name, presence: true, length: { in: 3..12 }
   has_many :posts
   has_many :comments
 end
